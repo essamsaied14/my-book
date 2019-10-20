b31552a6e1df04cca7ca0a4bdb69a6f0eb9e92f5
Essam Saied <essam.saied@elite-1st.com>
2019-10-20 19:46:32 +0200

    router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <site-header></site-header>
-    <site-carousel></site-carousel>
-    <site-products></site-products>
-    <updates-notification></updates-notification>
-    <featured-products></featured-products>
-    <contact-us></contact-us>
+    <!-- <site-carousel></site-carousel>
+    <site-products></site-products>-->
+    <!-- <updates-notification></updates-notification> -->
+    <!-- <featured-products></featured-products> -->
+    <!-- <contact-us></contact-us> -->
     <router-view />
     <site-footer></site-footer>
   </div>
